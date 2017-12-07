f35f7aac5dc32272d73e3f00a837e14cc8384437
John Desrosiers <johndrunner@yahoo.com>
2017-12-07 14:04:35 -0800

    added jumbotron and buttons with starter for button click handler

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,14 @@
-$(() => {
-  if (typeof $().modal === 'function') console.log('Bootstrap and jQuery Loaded');
-});
+// Function to handle user selection (rock, paper, or scissors chosen)
+function handleSelection() {
+  alert('chose ' + $(this).val());
+}
+
+
+// player clicks play button to start the game
+// prompt player to choose rock, paper, scissors
+// get the choice when the user clicks on one of the options
+$('.selection').on('click', handleSelection);
+// post choice to database
+// when opponent makes a choice
+// determine winner
+// update score and display message for the result of the match

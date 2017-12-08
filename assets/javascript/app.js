48a5cf049e84cb13ae400ceb636a221c7194b1ff
John Desrosiers <johndrunner@yahoo.com>
2017-12-08 07:18:40 -0800

    updates html for player and opponent choice when button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,34 @@
+// global vars
+const player = { choice: '' };
+const opponent = { choice: '' };
+
+function setChoice(id, value) {
+  $(id)
+    // update the class
+    .removeClass('rock paper scissors')
+    .addClass(value)
+    .children()
+    .first() // the span containing the text is the first child
+    .text(value);
+}
+
+function renderGame() {
+  // set the text and class for the player choice;
+  setChoice('#playerChoice', player.choice);
+  setChoice('#oppChoice', opponent.choice);
+}
+
 // Function to handle user selection (rock, paper, or scissors chosen)
 function handleSelection() {
-  alert('chose ' + $(this).val());
+  const choices = ['rock', 'paper', 'scissors'];
+  player.choice = $(this).val();
+
+  // temporarily use computer as the opponent
+  opponent.choice = choices[Math.floor(Math.random() * choices.length)];
+
+  // post choice to database
+  // update html for player choice
+  renderGame();
 }
 
 
@@ -8,7 +36,7 @@ function handleSelection() {
 // prompt player to choose rock, paper, scissors
 // get the choice when the user clicks on one of the options
 $('.selection').on('click', handleSelection);
-// post choice to database
+
 // when opponent makes a choice
 // determine winner
 // update score and display message for the result of the match

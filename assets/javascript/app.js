66576c125c0853a9fdb12a007d5adfd641e33bb1
John Desrosiers <johndrunner@yahoo.com>
2017-12-10 07:44:05 -0800

    added pseudo code; modified join game; added turn object

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,9 +17,57 @@ let playerNum = 0;
 const db = firebase.database();
 const player1Ref = db.ref('/player1');
 const player2Ref = db.ref('/player2');
+const turnRef = db.ref('/turn');
+
+// turn object
+const turn = (function Turn() {
+  let turnNum = 0;
+  let isInitialized = false;
+  let runOnChange = null;
+
+  // function to set a callback to be run when turn is changed
+  const onChange = (callback) => {
+    if (typeof callback === 'function') runOnChange = callback;
+    if (!callback) runOnChange = null;
+    return this;
+  };
+  const set = (val) => {
+    if (isInitialized) turnRef.set(val);
+  };
+  const increment = () => {
+    set(turnNum + 1);
+    return turnNum;
+  };
+
+  // Function sets intial state of turn
+  const init = () => {
+    // only runs if turn hasn't been initialized
+    if (isInitialized) return null;
+    isInitialized = true;
+
+    // set turn to 0 on disconnect
+    turnRef.onDisconnect().set(0);
+
+    // reset turn number and listen for changes in turn ref
+    turnRef.set(0);
+    return turnRef.on('value', (snap) => {
+      turnNum = parseInt(snap.val(), 10);
+      if (typeof runOnChange === 'function') runOnChange(turnNum);
+    });
+  };
+
+  // return public methods
+  return {
+    increment,
+    init,
+    onChange,
+    set,
+    val: () => turnNum,
+  };
+}());
 
 // ui objects
-function createPlayerView(choiceId, scoreId) {
+function createPlayerView(choiceId, scoreId, nameId) {
   return {
     show: () => $(choiceId).addClass('in'),
     hide: () => $(choiceId).removeClass('in'),
@@ -32,11 +80,12 @@ function createPlayerView(choiceId, scoreId) {
         .first()
         .text(choice);
     },
+    setName: name => $(nameId).text(name),
     setScore: wins => $(scoreId).text(wins),
   };
 }
-const playerView = createPlayerView('#playerChoice', '#playerWins');
-const opponentView = createPlayerView('#oppChoice', '#oppWins');
+const playerView = createPlayerView('#playerChoice', '#playerWins', '#playerName');
+const opponentView = createPlayerView('#oppChoice', '#oppWins', '#oppName');
 
 // Function randomly returns 'rock', 'paper', or 'scissors'
 function getRandomChoice() {
@@ -66,6 +115,7 @@ function waitForOpponent() {
       if (snap.exists()) {
         opponent.userName = snap.val().userName;
         player2Ref.off('value');
+        opponentView.setName(opponent.userName);
       }
     });
   } else if (playerNum === 2) {
@@ -73,6 +123,7 @@ function waitForOpponent() {
       if (snap.exists()) {
         opponent.userName = snap.val().userName;
         player1Ref.off('value');
+        opponentView.setName(opponent.userName);
       }
     });
   }
@@ -90,7 +141,7 @@ function joinGame() {
         playerNum = 1;
       }
     })
-    // else if there is no player2 connected then player join game as player2
+    // else if there is no player2 connected then player joins game as player2
     .then(() => player2Ref.once('value'))
     .then((snap) => {
       if (!snap.exists() && !playerNum) {
@@ -99,8 +150,26 @@ function joinGame() {
         playerNum = 2;
       }
     })
-    // get opponent info once opponent joins
-    .then(waitForOpponent);
+    .then(() => {
+      // if playerNum is truthy
+      if (playerNum) {
+        // hide sign in modal and display userName
+        playerView.setName(player.userName);
+        $('#startModal').modal('hide');
+        // TODO display welcome status message
+        // TODO push player joined game message to chat
+      }
+
+      // if player joined game and opponent hasn't
+      if (playerNum && turn.val() === 0) {
+        // TODO display waiting for opponent status
+      }
+
+      // if playerNum = truthy and other player is set && turn = 0
+      if (playerNum && turn.val() === 1) {
+        // set turn to 1
+      }
+    });
 }
 
 // get the choice when the user clicks on one of the options
@@ -147,21 +216,69 @@ $('.selection').on('click', function handleSelectionBtnClick() {
   }
 });
 
-// display sign in at first
+// display sign in at first to get userName input
 $('#startModal').modal({ backdrop: 'static' });
 
 // when user clicks start
 $('#signInForm').on('submit', (event) => {
   event.preventDefault();
+
+  // hide unable to join alert if its showing
   $('#joinAlert').removeClass('in');
+
+  // get user input and see if user is able to join the game
   player.userName = $('#txtUserName').val().trim();
   joinGame()
     .then(() => {
       if (playerNum) {
-        $('#startModal').modal('hide');
+        // user joined game, update the display
+
       } else {
+        // unable to join game. display alert
         $('#joinAlert').addClass('in');
       }
     })
     .catch(console.log);
 });
+
+
+
+/* Pseudocode ------------------
+
+user enters name
+if player1 not set, playerNum = 1
+if player1 is set and player2 is not set, playerNum = 2
+if player1 && player2 are both set, player may not join game
+
+if playerNum is truthy
+  hide sign in modal
+  display player name
+  display status message
+  push player joined game message to chat
+
+if playerNum = truthy && turn = 0
+  display waiting for player to join message
+
+if playerNum = truthy and other player is set && turn = 0
+  set turn = 1
+
+when user clicks a choice
+  push game event message to chat
+  increment turn
+  display the user's choice
+
+when turn changes
+  if turn = 1
+    display the opponents name
+  if turn = 2 and player.choice is truthy
+    display waiting for opponent to choose msg
+  if turn = 3
+    display opponent choice
+    determine winner
+    push game event message to chat
+    display modal with result for 3 seconds
+    reset player values (not the display)
+
+when the result modal time is up
+  reset the display
+ */

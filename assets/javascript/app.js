74a4bb15aeded81c5bb4ee23e914aedcfd67beb2
John Desrosiers <johndrunner@yahoo.com>
2017-12-09 17:27:42 -0800

    added handling of player choice

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // global vars
-const opponent = {
+let opponent = {
   userName: '',
   choice: '',
   wins: 0,
@@ -17,6 +17,26 @@ let playerNum = 0;
 const db = firebase.database();
 const player1Ref = db.ref('/player1');
 const player2Ref = db.ref('/player2');
+
+// ui objects
+function createPlayerView(choiceId, scoreId) {
+  return {
+    show: () => $(choiceId).addClass('in'),
+    hide: () => $(choiceId).removeClass('in'),
+    setChoice: (choice) => {
+      console.log('setChoice called', choice);
+      $(choiceId)
+        .removeClass('paper rock scissors')
+        .addClass(choice)
+        .children()
+        .first()
+        .text(choice);
+    },
+    setScore: wins => $(scoreId).text(wins),
+  };
+}
+const playerView = createPlayerView('#playerChoice', '#playerWins');
+const opponentView = createPlayerView('#oppChoice', '#oppWins');
 
 // Function randomly returns 'rock', 'paper', or 'scissors'
 function getRandomChoice() {
@@ -40,6 +60,25 @@ function getWinner() {
   return 'player';
 }
 
+function waitForOpponent() {
+  if (playerNum === 1) {
+    player2Ref.on('value', (snap) => {
+      if (snap.exists()) {
+        opponent.userName = snap.val().userName;
+        player2Ref.off('value');
+      }
+    });
+  } else if (playerNum === 2) {
+    player1Ref.on('value', (snap) => {
+      if (snap.exists()) {
+        opponent.userName = snap.val().userName;
+        player1Ref.off('value');
+      }
+    });
+  }
+}
+
+// Function to handle joining game if possible
 function joinGame() {
   // if there is no player1 connected then player joins game as player1
   return player1Ref
@@ -59,16 +98,53 @@ function joinGame() {
         player2Ref.onDisconnect().remove();
         playerNum = 2;
       }
-    });
-}
-
-function runGame() {
-
+    })
+    // get opponent info once opponent joins
+    .then(waitForOpponent);
 }
 
 // get the choice when the user clicks on one of the options
 $('.selection').on('click', function handleSelectionBtnClick() {
-  
+  if (!player.choice) {
+    player.choice = $(this).val();
+    // update database
+    if (playerNum === 1) {
+      player1Ref.update(player);
+    } else {
+      player2Ref.update(player);
+    }
+    // render the players choice
+    playerView.hide();
+    playerView.setChoice(player.choice);
+    playerView.show();
+
+    // wait for opponent to make choice
+    if (playerNum === 1) {
+      player2Ref.on('value', (snap) => {
+        if (snap.exists()) opponent = snap.val();
+        if (opponent.choice) {
+          // render the choice
+          opponentView.hide();
+          opponentView.setChoice(opponent.choice);
+          opponentView.show();
+          player2Ref.off('value');
+          // resolve game
+        }
+      });
+    } else if (playerNum === 2) {
+      player1Ref.on('value', (snap) => {
+        if (snap.exists()) opponent = snap.val();
+        if (opponent.choice) {
+          // render the choice
+          opponentView.hide();
+          opponentView.setChoice(opponent.choice);
+          opponentView.show();
+          player1Ref.off('value');
+          // resolve game
+        }
+      });
+    }
+  }
 });
 
 // display sign in at first
@@ -81,8 +157,11 @@ $('#signInForm').on('submit', (event) => {
   player.userName = $('#txtUserName').val().trim();
   joinGame()
     .then(() => {
-      if (playerNum) $('#startModal').modal('hide');
-      else $('#joinAlert').addClass('in');
+      if (playerNum) {
+        $('#startModal').modal('hide');
+      } else {
+        $('#joinAlert').addClass('in');
+      }
     })
     .catch(console.log);
 });
